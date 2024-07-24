67d3dbfb47aa9fa4b30fa7992fb23ffc14868418
fechhh <federico.grijalba@gmail.com>
2024-07-24 11:09:09 +0000

    working

--- src/workflows/719_workflow_epic_f202109_completo_v1.r
+++ src/workflows/719_workflow_epic_f202109_completo_v1.r
@@ -263,25 +263,45 @@ TS_strategy_base9 <- function( pinputexps )
 
   param_local$future <- c(202109)
 
-  param_local$final_train$undersampling <- 1.0
-  param_local$final_train$clase_minoritaria <- c("BAJA+2")  # VERRRRRRRRRR
-  param_local$final_train$training <- c(201905, 201906, 201907, 201908, 201909,
-                                        201910, 201911, 201912, 202001, 202002,
-                                        202008, 202009, 202010, 202011, 202012,
-                                        202101, 202104, 202106)  # VERRRRRRRRRRRR
+  #param_local$final_train$undersampling <- 1.0
+  #param_local$final_train$clase_minoritaria <- c("BAJA+2")  # VERRRRRRRRRR
+  #param_local$final_train$training <- c(201905, 201906, 201907, 201908, 201909,
+  #                                      201910, 201911, 201912, 202001, 202002,
+  #                                      202008, 202009, 202010, 202011, 202012,
+  #                                      202101, 202104, 202106)  # VERRRRRRRRRRRR
   
-  param_local$train$training <- c(201905, 201906, 201907, 201908, 201909,
-                                  201910, 201911, 201912, 202001, 202002,
-                                  202008, 202009, 202010, 202011, 202012,
-                                  202101, 202104, 202106)
-  param_local$train$validation <- c(202102)
-  param_local$train$testing <- c(202103, 202105, 202107)
-
+  #param_local$train$training <- c(201905, 201906, 201907, 201908, 201909,
+  #                                201910, 201911, 201912, 202001, 202002,
+  #                                202008, 202009, 202010, 202011, 202012,
+  #                                202101, 202104, 202106)
+  #param_local$train$validation <- c(202102)
+  #param_local$train$testing <- c(202103, 202105, 202107)
 
   # Atencion  0.2  de  undersampling de la clase mayoritaria,  los CONTINUA
   # 1.0 significa NO undersampling
+  #param_local$train$undersampling <- 0.2
+  #param_local$train$clase_minoritaria <- c( "BAJA+1")  # VERRRRRRRRRR
+  
+  
+  
+  param_local$final_train$undersampling <- 1.0
+  param_local$final_train$clase_minoritaria <- c( "BAJA+1", "BAJA+2")
+  param_local$final_train$training <- c(202107, 202106, 202105, 202104, 202103, 202102,
+                                        202101, 202012, 202011)
+  
+  
+  param_local$train$training <- c(202105, 202104, 202103, 202102, 202101,
+                                  202012, 202011, 202010, 202009)
+  param_local$train$validation <- c(202106)
+  param_local$train$testing <- c(202107)
+  
+  
+  # Atencion  0.2  de  undersampling de la clase mayoritaria,  los CONTINUA
+  # 1.0 significa NO undersampling
   param_local$train$undersampling <- 0.2
-  param_local$train$clase_minoritaria <- c( "BAJA+1")  # VERRRRRRRRRR
+  param_local$train$clase_minoritaria <- c( "BAJA+1", "BAJA+2")
+  
+  
 
   return( exp_correr_script( param_local ) ) # linea fija
 }
