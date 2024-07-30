17e1eaf76d38e56cc63f289f5a6d7462f359d9fb
fechhh <federico.grijalba@gmail.com>
2024-07-30 13:10:51 +0000

    u

--- src/workflows/719_workflow_epic_f202109_completo_v8.r
+++ src/workflows/719_workflow_epic_f202109_completo_v8.r
@@ -429,7 +429,7 @@ wf_septiembre <- function( pnombrewf )
   param_local <- exp_wf_init( pnombrewf ) # linea fija
 
   DT_incorporar_dataset_competencia2024()
-  CA_catastrophe_base( metodo="ninguno")
+  CA_catastrophe_base( metodo="MachineLearning")
   FEintra_manual_base()
   DR_drifting_base(metodo="ninguno")
   FEhist_base()
